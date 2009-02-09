82707ecdbb53d780346d37932bccdb4b3adaf624
lehphyro <lehphyro@c46cf2fc-f51e-11dd-99c0-75d572ba1ddd>
2009-02-09 01:19:18 +0000

    [maven-release-plugin]  copy for tag maven-config-processor-plugin-1.1

---
+++ tags/maven-config-processor-plugin-1.1/src/main/java/com/google/code/configprocessor/ExpressionResolver.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009 Leandro de Oliveira Aparecido <lehphyro@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.code.configprocessor;
+
+import org.codehaus.plexus.component.configurator.expression.*;
+
+public class ExpressionResolver {
+
+	private ExpressionEvaluator evaluator;
+	
+	public ExpressionResolver(ExpressionEvaluator evaluator) {
+		this.evaluator = evaluator;
+	}
+	
+	public String resolve(String value) {
+		try {
+			Object resolvedValue = evaluator.evaluate(value);
+			if (resolvedValue != null && !(resolvedValue instanceof String)) {
+				throw new IllegalArgumentException("Expression [" + value + "] did not resolve to String");
+			}
+			return (String)resolvedValue;
+		} catch (ExpressionEvaluationException e) {
+			throw new RuntimeException("Error resolving expression [" + value + "]", e);
+		}
+	}
+	
+}
---
+++ tags/maven-config-processor-plugin-1.1/src/main/java/com/google/code/configprocessor/processing/xml/AbstractXmlActionProcessingAdvisor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2009 Leandro de Oliveira Aparecido <lehphyro@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.code.configprocessor.processing.xml;
+
+import javax.xml.namespace.*;
+import javax.xml.xpath.*;
+
+import org.w3c.dom.*;
+
+import com.google.code.configprocessor.*;
+
+public abstract class AbstractXmlActionProcessingAdvisor implements XmlActionProcessingAdvisor {
+	
+	private ExpressionResolver expressionResolver;
+	private NamespaceContext namespaceContext;
+	private XPathExpression xpathExpression;
+	
+	public AbstractXmlActionProcessingAdvisor(ExpressionResolver expressionResolver, NamespaceContext namespaceContext) {
+		this.expressionResolver = expressionResolver;
+		this.namespaceContext = namespaceContext;
+	}
+	
+	protected void compile(String expression) throws ParsingException {
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		xpath.setNamespaceContext(namespaceContext);
+		try {
+			xpathExpression = xpath.compile(resolve(expression));
+		} catch (XPathExpressionException e) {
+			throw new ParsingException(e);
+		}
+	}
+	
+	protected Node evaluateForSingleNode(Document document) throws ParsingException {
+		try {
+			Node node = (Node)getXPathExpression().evaluate(document, XPathConstants.NODE);
+			if (node == null) {
+				throw new ParsingException("XPath expression did not find nodes: " + getXPathExpression().toString());
+			}
+
+			Node parent = node.getParentNode();
+			if (parent == null) {
+				throw new ParsingException("Cannot manipulate node without a parent");
+			}
+			
+			return node;
+		} catch (XPathExpressionException e) {
+			throw new ParsingException(e);
+		}
+	}
+	
+	protected String resolve(String value) {
+		return expressionResolver.resolve(value);
+	}
+	
+	protected XPathExpression getXPathExpression() {
+		return xpathExpression;
+	}
+}
